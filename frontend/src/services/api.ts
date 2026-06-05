a87c77996f3b69242e4f859334bcbb19f84bf012
Gilka Lorena Kuno Apaza <gilkakuno@gmail.com>
2026-06-05 11:47:53 -0400

    .

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API_URL = (import.meta as any).env?.VITE_API_URL || '/api'
+const API_URL = (import.meta as any).env?.PROD
+  ? 'https://plomeria-backend.onrender.com'
+  : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: API_URL,
